b546d362a33d9be45f5c71ee23f5ffc3b2ceaad2
dharmendra1234 <dharmendra.pawar@gmail.com>
2017-04-15 01:40:50 -0500

    Removed Style

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -6,7 +6,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css'],
   providers :[EmployeeService]
 })
 export class AddComponent implements OnInit {
